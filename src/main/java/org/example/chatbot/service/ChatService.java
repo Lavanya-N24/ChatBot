d888aa06e1c57051a26cd0121428b0d87c3cb4d7
Lavanya N M <lavanyanm75@gmail.com>
2026-09-11 22:47:52 +0530

    floorandceil

--- src/main/java/org/example/chatbot/service/ChatService.java
+++ src/main/java/org/example/chatbot/service/ChatService.java
@@ -1,12 +1,26 @@
 package org.example.chatbot.service;
 
+import org.example.chatbot.entity.ChatMessage;
+import org.example.chatbot.repository.ChatMessageRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatService {
-    public String generateResponse(String message){
-        return "you said:" +message;
+
+    private final ChatMessageRepository chatMessageRepository;
+
+    public ChatService(ChatMessageRepository chatMessageRepository) {
+        this.chatMessageRepository = chatMessageRepository;
     }
 
+    public String generateResponse(String message) {
 
+        String response = "You said: " + message;
+
+        ChatMessage chatMessage = new ChatMessage(message, response);
+
+        chatMessageRepository.save(chatMessage);
+
+        return response;
+    }
 }
